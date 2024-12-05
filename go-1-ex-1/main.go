5bf900c20442973cfd93a6272376dfd8f7880af1
Dylan Bellomusto <dylan_bellomusto@sluz.ch>
2024-12-05 20:38:41 +0100

    go-1-ex-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: Declare and initialize the variables being used in the output!
+
+	var firstName string = "Dylan"
+	var lastName string = "Bellomusto"
+	var dayOfBirth int = 27
+	var monthOfBirth int = 5
+	var yearOfBirth int = 2008
+	var numberOfSiblings int = 2
+	var heightInMeters float64 = 1.70
+	var zodiacSign rune = '\u264a'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
