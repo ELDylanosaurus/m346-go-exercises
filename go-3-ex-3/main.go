a727b37dc47854bb4d0e5e343b2cd55574166807
Dylan Bellomusto <Dylan_Bellomusto@sluz.ch>
2024-12-06 13:35:35 +0100

    go-3-ex-2,3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,10 +1,36 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
 )
 
 func main() {
-	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
+	fmt.Println("\nFizzBuzz using switch/case:")
+	for i := Lower; i <= Upper; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
