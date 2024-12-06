19d975226c5522dfc520765ba077550e16cdff06
Dylan Bellomusto <Dylan_Bellomusto@sluz.ch>
2024-12-06 12:48:43 +0100

    go-2-ex-3,4

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+	modules := map[int]string{
+		104: "Einführung in die Programmierung",
+		117: "Algorithmen und Datenstrukturen",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 117) 
+
+	modules[220] = "Webentwicklung mit Go" 
+
+	modules[346] = "DevOps und Cloud-Management" 
+
 	fmt.Println(modules)
 }
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,49 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Name    string
+	Students []Student
+}
+
 func main() {
-	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
-	// TODO: declare a map of modules being attended by multiple classes
-	// TODO: output everything using fmt.Println()
+	student1 := Student{FirstName: "Dylan", LastName: "Bellomusto"}
+	student2 := Student{FirstName: "Nando", LastName: "Brun"}
+	student3 := Student{FirstName: "Leandro", LastName: "Schweggler"}
+	student4 := Student{FirstName: "Lisa", LastName: "Eggerschwiler"}
+	student5 := Student{FirstName: "Peter", LastName: "Kauffmann"}
+	student6 := Student{FirstName: "Sophie", LastName: "Hegi"}
+
+	classA := Class{
+		Name: "Class A",
+		Students: []Student{student1, student2, student3},
+	}
+	classB := Class{
+		Name: "Class B",
+		Students: []Student{student4, student5, student6},
+	}
+
+	modules := map[int][]Class{
+		104: {classA, classB}, 
+		117: {classA},         
+		346: {classB},         
+	}
+
+	fmt.Println("Classes and their students:")
+	fmt.Println(classA)
+	fmt.Println(classB)
+
+	fmt.Println("\nModules and attending classes:")
+	for module, classes := range modules {
+		fmt.Printf("Module %d:\n", module)
+		for _, class := range classes {
+			fmt.Printf("  %s with students: %v\n", class.Name, class.Students)
+		}
+	}
 }
