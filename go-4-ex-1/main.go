1c1a3ac0c3176b6ebc237de9341f4f4bfe20ba79
Dylan Bellomusto <Dylan_Bellomusto@sluz.ch>
2024-12-13 12:40:43 +0100

    go-4-ex-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,37 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+)
+
+func computeGrade(gotPoints, maxPoints float64) (float64, error) {
+	if gotPoints < 0 || maxPoints <= 0 || gotPoints > maxPoints {
+		return 0, errors.New("invalid points: gotPoints must be between 0 and maxPoints, and maxPoints must be positive")
+	}
+
+	grade := 1.0 + 5.0*(gotPoints/maxPoints)
+	return grade, nil
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	grades := []struct {
+		gotPoints float64
+		maxPoints float64
+	}{
+		{17.5, 28.0}, // 4.1
+		{25.0, 30.0}, // 5.2
+		{0.0, 20.0},  // 1.0 
+		{-5.0, 30.0}, // Error
+		{35.0, 30.0}, // Error
+	}
+
+	for _, g := range grades {
+		grade, err := computeGrade(g.gotPoints, g.maxPoints)
+		if err != nil {
+			fmt.Printf("Error for %v/%v: %v\n", g.gotPoints, g.maxPoints, err)
+		} else {
+			fmt.Printf("Grade for %v/%v: %.3f\n", g.gotPoints, g.maxPoints, grade)
+		}
+	}
 }
